4defa166a7f112a7441552690537cab6367fdd18
falovezjy <114210457+falovezjy@users.noreply.github.com>
2022-09-23 14:25:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -57,7 +57,7 @@ export const USER_CONFIG = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [omlYI6OxMT9K2cUdnCcxVYsMnJ_8]
+  USERS: [feelyyds]
     {
       // 想要发送的人的名字
       name: "张嘉怡",
