aad4003c095f40ea947b0e9e7046855547b25e0e
falovezjy <114210457+falovezjy@users.noreply.github.com>
2022-09-23 14:52:39 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -57,7 +57,7 @@ export const USER_CONFIG = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [feelyyds]
+  USERS: [
     {
       // 想要发送的人的名字
       name: "张嘉怡",
@@ -78,15 +78,23 @@ export const USER_CONFIG = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "2003", "date": "02-06"},
-        {"type": "节日", "name": "恋爱纪念日", "year": "2022", "date": "09-12"},
+        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
+        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "费傲", "year": "2004", "date": "02-28"},
-        {"type": "节日", "name": "被搭讪纪念日", "year": "2022", "date": "09-09"},
-      
+        {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
+        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+      ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-      customizedDateList: []
-
+      customizedDateList: [
+        // 在一起的日子
+        {"keyword": "love_day", date: "2022-09-08"},
+        // 结婚纪念日
+        {"keyword": "marry_day", date: "2022-09-09"},
+        // 退伍日
+        {"keyword": "ex_day", date: "2022-09-10"},
+      ]
+    },
+    {
       name: "老婆1",
       id: "",
       useTemplateId: "",
@@ -232,3 +240,17 @@ export const USER_CONFIG = {
   
   
 }
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
